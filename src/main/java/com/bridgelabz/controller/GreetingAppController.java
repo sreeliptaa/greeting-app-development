410d399a0d090dd8e4d4f5b81024f019ee0a041d
sreeliptaa <sreelipta65@gmail.com>
2021-12-03 14:24:30 +0530

    Added : Methods to display greeting messages based on user provided attributes

--- src/main/java/com/bridgelabz/controller/GreetingAppController.java
+++ src/main/java/com/bridgelabz/controller/GreetingAppController.java
@@ -3,6 +3,7 @@ package com.bridgelabz.controller;
 import com.bridgelabz.service.GreetingAppService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,16 +22,28 @@ public class GreetingAppController {
 
     @GetMapping(value = "/message")
     public String greeting() {
+
         return "Hello, Welcome to our Greeting App.";
     }
 
     @GetMapping(value = "/message2")
     public String greeting(@RequestParam String name) {
+
         return "Hello " + name + ", Welcome to our Greeting App.";
     }
 
     @GetMapping(value = "/message3")
     public String helloGreeting() {
         return greetingAppService.helloGreeting();
+    }
+
+    @GetMapping(value = "/message4/{name}")
+    public String greetingMessage(@PathVariable String name) {
+        return "Hello " + name ;
+    }
+
+    @GetMapping(value = "/message5")
+    public String greeting(@RequestParam String firstName, @RequestParam String lastName) {
+        return "Hello " + firstName + " " + lastName;
     }
 }
--- src/main/java/com/bridgelabz/service/GreetingAppService.java
+++ src/main/java/com/bridgelabz/service/GreetingAppService.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class GreetingAppService {
-    public String helloGreeting(){
+    public String helloGreeting() {
         return "Hello World";
     }
 }
